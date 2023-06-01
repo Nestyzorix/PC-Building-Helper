557c1b3c4c0a422eadc3cfe103a3918324d60f3e
Maxim Suhomlin <sukhomlinm@mail.ru>
2023-06-01 23:17:49 +0300

    register models in admin panel

--- PC_Building_site/backend_api/admin.py
+++ PC_Building_site/backend_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from backend_api.models import *
 
-# Register your models here.
+
+admin.site.register(Processor)
+admin.site.register(CoolingSystem)
+admin.site.register(Motherboard)
+admin.site.register(RAM)
+admin.site.register(Videocard)
+admin.site.register(HDD)
+admin.site.register(SSD)
+admin.site.register(PowerUnit)
